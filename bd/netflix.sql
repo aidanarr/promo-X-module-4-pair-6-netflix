5411d0710e3185e72b10f8734aed3f73289c430d
Your Name <you@example.co>
2024-05-29 11:24:26 +0200

    ej 2

--- bd/netflix.sql
+++ bd/netflix.sql
@@ -26,3 +26,12 @@ CREATE TABLE Actors (
     country VARCHAR(45) not null,
     birthday date not null
 );
+INSERT into Movies (title,genre,image,category,`year`)
+values ("Mamma Mia","Musical","https://upload.wikimedia.org/wikipedia/en/a/a6/MammaMiaTeaserPoster.JPG","Top 10", 2008);
+
+SELECT title, genre FROM Movies where `year`> 2000; 
+SELECT * FROM Movies where genre= "Comedia";
+UPDATE Movies set `year`= 1997 where title = "La vita è bella";
+
+SELECT * FROM actors where birthday > "1950-01-01" and birthday< "1960-12-31";
+DELETE FROM Users where `name` like "M%";
