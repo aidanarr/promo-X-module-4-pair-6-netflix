84eb035eea832c1d89741efbd4192ab2d2991467
Diana Bernardos <dianilla75@gmail.com>
2024-06-06 11:43:55 +0200

    params

--- src/index.js
+++ src/index.js
@@ -27,28 +27,28 @@ async function connectBD() {
 }
 
 server.get('/movies', async (req,res)=>{
-  const fakeMovies = [
-    {
-      id: 1,
-      title: "Wonder Woman",
-      genre: "Action",
-      image:
-        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-      category: "Superhero",
-      year: 2017,
-      director: "Patty Jenkins",
-    },
-    {
-      id: 2,
-      title: "Inception",
-      genre: "Science Fiction",
-      image:
-        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-      category: "Thriller",
-      year: 2010,
-      director: "Christopher Nolan",
-    },
-  ];
+  // const fakeMovies = [
+  //   {
+  //     id: 1,
+  //     title: "Wonder Woman",
+  //     genre: "Action",
+  //     image:
+  //       "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+  //     category: "Superhero",
+  //     year: 2017,
+  //     director: "Patty Jenkins",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Inception",
+  //     genre: "Science Fiction",
+  //     image:
+  //       "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+  //     category: "Thriller",
+  //     year: 2010,
+  //     director: "Christopher Nolan",
+  //   },
+  // ];
   const conn = await connectBD();
 
   const selectMovies = "SELECT * FROM Movies;"
